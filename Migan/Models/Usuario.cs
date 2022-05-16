532f4196b8f5b75004ab1c3a6699649b34f16b74
Chamo 3000 <Chamo3000@gmail.com>
2022-05-16 17:03:18 -0500

    Asignacion Elementos del usuario inicial

--- Migan/Models/Usuario.cs
+++ Migan/Models/Usuario.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Migan.Models
 {
+    [Table("Usuario", Schema = "dbo")]
     public partial class Usuario
     {
+        [Key]
         public int Id { get; set; }
+        [Display(Name = "Nombre de Usuario")] 
         public string? Usuario1 { get; set; }
+        [Display(Name = "Contraseña")]
+        [DataType(DataType.Password)]
         public string? Contrasena { get; set; }
         public string? Nombre { get; set; }
         public string? Apellido { get; set; }
--- Migan/Program.cs
+++ Migan/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Migan.Models;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<MIGANContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conexion"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
