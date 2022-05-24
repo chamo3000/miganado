34b0861958bdaa77de6c1430dc8321aa2eda3a0b
Chamo 3000 <Chamo3000@gmail.com>
2022-05-23 21:08:22 -0500

    Adicion Controladores

---
+++ Migan/Controllers/SexoController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Migan.Controllers
+{
+    public class SexoController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
